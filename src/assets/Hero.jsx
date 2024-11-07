2204ba2303827830fc2845fc1383f93bebd3b58e
uthumWijenayake <uthumwijenayake@gmail.com>
2024-11-07 16:10:46 +0530

    completed hero

--- src/assets/Hero.jsx
+++ src/assets/Hero.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import './Hero.css';
-import heroimg from '../assets/heroPng.jpeg';
+import '../assets/hero.css';
+
+// import heroimg from '../assets/heroPng.jpeg';
+// import hero_img from '../assets/hero_img.jpg';
+import hero_women from '../assets/women_img.jpg';
 
 const Hero = () => {
   return (
-    <div className='hero-img'>
-      <img src={heroimg} alt="" />
+    <div className='hero'>
+      <img src={hero_women} alt="" />
+      <h1><span>"Where <br/> Style<br/> Meets <br/>Comfort"</span></h1>
+      <h6>The Fashion Shop 2025</h6>
     </div>
   );
 };
--- src/assets/Nav.jsx
+++ src/assets/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Components/Nav.css';
-import navLogo from './navLogo.png';
+// import navLogo from './navLogo.png';
+import newLogo from '../assets/logo_new.png';
 
 const Nav = () => {
   return (
     <div className='nav-bar'>
       <div className="nav-logo">
-         <img src={navLogo} alt="" />
+         <img src={newLogo} alt="" />
       </div>
       <ul className='nav-menu'>
         <li>Home</li>
